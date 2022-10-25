dcb78a10f23d269366b77ceb647f264c36aed1ba
AlimunNahar <alimunnaharmitu@gmail.com>
2022-10-25 23:02:38 +0600

    error solved

--- index.js
+++ index.js
@@ -12,17 +12,30 @@ app.get("/", (req, res) => {
   res.send("Courses API Running!");
 });
 
-app.get("/course-categories", (req, res) => {
+app.get("/course-name", (req, res) => {
   res.send(courses);
+});
+// 08/07 => id like this
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "07") {
+    res.send(courseDetails);
+  } else {
+    const course_details = courseDetails.filter((c) => c.id === id);
+    res.send(course_details);
+  }
 });
 
 app.get("/details", (req, res) => {
   res.send(courseDetails);
 });
 
+// id => 0282e0e58a5c404fbd15261f11c2ab6a
 app.get("/details/:id", (req, res) => {
   const id = req.params.id;
-  const selectedCourse = news.find((n) => n._id === id);
+  const selectedCourse = courseDetails.find(
+    (course) => course.course_id === id
+  );
   res.send(selectedCourse);
 });
 
