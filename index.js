4032fc4ef987233d55237e33c08741dc81114149
AlimunNahar <alimunnaharmitu@gmail.com>
2022-10-25 20:18:17 +0600

    dynamic api created

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get("/details", (req, res) => {
   res.send(courseDetails);
 });
 
+app.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = news.find((n) => n._id === id);
+  res.send(selectedCourse);
+});
+
 app.listen(port, () => {
   console.log(`ThryVI educational Server running on port: ${port}`);
 });
