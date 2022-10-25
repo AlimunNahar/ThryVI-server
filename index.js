622a0adbf199236cb5da706c6b0891cbd4236d15
AlimunNahar <alimunnaharmitu@gmail.com>
2022-10-25 20:14:49 +0600

    express and cors installed

--- index.js
+++ index.js
@@ -4,28 +4,3 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
-const courses = require("./data/courses.json");
-const courseDetails = require("./data/details.json");
-
-app.get("/", (req, res) => {
-  res.send("Courses API Running!");
-});
-
-app.get("/course-categories", (req, res) => {
-  res.send(courses);
-});
-
-app.get("/details", (req, res) => {
-  res.send(courseDetails);
-});
-
-app.get("/details/:id", (req, res) => {
-  const id = req.params.id;
-  const selectedCourse = news.find((n) => n._id === id);
-  res.send(selectedCourse);
-});
-
-app.listen(port, () => {
-  console.log(`ThryVI educational Server running on port: ${port}`);
-});
